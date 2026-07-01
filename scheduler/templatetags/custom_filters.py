20da9139bfebe9bede925f6ae1d1685f977e7f95
coodgeek2005 <muhammadbinramzan2005@gmail.com>
2026-07-01 22:06:04 +0500

    Update custom filters to handle error if non-dict is provided as an argument

--- scheduler/templatetags/custom_filters.py
+++ scheduler/templatetags/custom_filters.py
@@ -3,4 +3,6 @@ register = template.Library()
 
 @register.filter
 def get_item(dictionary, key):
-    return dictionary.get(key)
+    if isinstance(dictionary, dict):
+        return dictionary.get(key)
+    return None
